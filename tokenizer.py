910237ff4a287a609f2febef4f6a2cb5ce89fc1e
aaron-jencks <aaronjencks@aol.com>
2025-09-25 23:42:34 -0400

    got full tokenizer to work

--- tokenizer.py
+++ tokenizer.py
@@ -2,13 +2,32 @@ import argparse
 import logging
 import os
 import pathlib
+import random
+from typing import List
 
+import numpy as np
 from tokenizers.implementations import ByteLevelBPETokenizer
+from tqdm import tqdm
 from transformers import GPT2TokenizerFast
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def flatten_documents(documents) -> List[int]:
+    result = []
+    for document in documents:
+        result.extend(document)
+    return result
+
+
+def save_data(split_data: List[int], split: str, directory: pathlib.Path):
+    logger.info(f'saving data for {split}...')
+    id_fname = directory / f'{split}.bin'
+    with open(id_fname, 'wb+') as fp:
+        iddata = np.array(split_data, dtype=np.uint16).tobytes()
+        fp.write(iddata)
 
 
 if __name__ == '__main__':
@@ -27,6 +46,8 @@ if __name__ == '__main__':
         help="The name of the output vocab files"
     )
     parser.add_argument('--cpus', type=int, default=os.cpu_count(), help='the number of cores to use')
+    parser.add_argument('--validation-ratio', type=float, default=0.2, help='the ratio of validation set size')
+    parser.add_argument('--token-directory', type=pathlib.Path, default=pathlib.Path("."), help='Path to the output token directory')
     args = parser.parse_args()
 
     logger.info('training tokenizer...')
@@ -62,7 +83,7 @@ if __name__ == '__main__':
     vocab_fname = args.output_directory / "{}-vocab.json".format(args.name)
     merges_fname = args.output_directory / "{}-merges.txt".format(args.name)
 
-    print(f'reading data from {str(vocab_fname)} and {str(merges_fname)}')
+    logger.info(f'reading data from {str(vocab_fname)} and {str(merges_fname)}')
 
     tokenizer = GPT2TokenizerFast(
         str(vocab_fname), str(merges_fname),
@@ -86,6 +107,22 @@ if __name__ == '__main__':
         lines = fp.read().splitlines(keepends=False)
 
     first_row_ids = tokenizer([lines[0]])['input_ids'][0]
-    print(f'First row encoded:')
-    print(f'ids: {first_row_ids}')
-    print(f'tokens: {tokenizer.convert_ids_to_tokens(first_row_ids)}')
+    logger.info(f'First row encoded:')
+    logger.info(f'ids: {first_row_ids}')
+    logger.info(f'tokens: {tokenizer.convert_ids_to_tokens(first_row_ids)}')
+
+    token_documents = []
+    for line in lines:
+        tokens = tokenizer([line])['input_ids'][0]
+        token_documents.append(tokens)
+
+    train_size = int(len(token_documents) * (1 - args.validation_ratio))
+    random.shuffle(token_documents)
+    train_documents = token_documents[:train_size]
+    valid_documents = token_documents[train_size:]
+
+    train_tokens = flatten_documents(train_documents)
+    valid_tokens = flatten_documents(valid_documents)
+
+    save_data(train_tokens, 'train', args.token_directory)
+    save_data(valid_tokens, 'valid', args.token_directory)
